f1ada19c542ed81dd6d6010bdf60c632d52f6f50
zen <artperillo.ictdu@spcf.edu.ph>
2022-08-29 20:27:34 +0800

    :tada: Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # todo-list
+
 Simple Todo List
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const name = "World"; 
+
+function App() 
+{
+	return (
+		<div className="App">
+			Hello World, { name }
+		</div>
+	);
+}
+
+export default App;
