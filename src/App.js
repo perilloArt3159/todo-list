70fb7f80802df9e4781995ffcc6247e0e1133105
zen <artperillo.ictdu@spcf.edu.ph>
2022-09-02 21:56:27 +0800

    :package: Added TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import Task from './components/Task'; 
 
 const name = "World"; 
 
-function App() 
+const App = () => 
 {
+	const showTask = true; 
+
 	return (
 		<div className="App">
+			<h1 className="text-3xl font-bold underline">
+				Hello world!
+			</h1>
 			Hello World, { name }
+
+			{ 
+			showTask 
+				? 
+					(
+						<>
+							<Task name="Chess"/>
+							<Task name="BasketBall"/>
+						</>
+					)
+				: 
+					<p> Not Authorized </p>
+			} 
 		</div>
+
 	);
 }
 
---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+const task = (props) => 
+{
+    return (
+        <div>
+            <h1> Sport { props.name } </h1>
+        </div>
+    );
+}
+
+export default task; 
